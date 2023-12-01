c79a74173be4ecdd5e4f191a4fc530e8caea26df
João Victor <silvasouzajoaovictor877@gmail.com>
2023-12-01 15:57:00 -0300

    new images

--- script.js
+++ script.js
@@ -16,4 +16,16 @@ function nextImage(){
 
 document.getElementById("radio"+count).checked = true;
 
+/*evento de clique a todos os link com # quando clicado correspondente a uma página de com efeito deslizante*/
 }
+    document.addEventListener("DOMContentLoaded", function () {
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+    });
