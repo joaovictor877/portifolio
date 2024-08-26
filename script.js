0417ad0046666eee783ae07ce49820f6e58450b1
Joao <silvasouzajoaovictor877@gmail.com>
2024-08-26 14:46:37 -0300

    tirado o efeito js do carrosel

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-/* slider-es animação em carrosel tech */
-let count = 1;
-document.getElementById("radio1").checked = true;
-
-setInterval( function(){
-nextImage();
-}, 5000)
-
-
-function nextImage(){
-    count++;
-    if(count>4){
-        count = 1;
-}
-
-document.getElementById("radio"+count).checked = true;
 
 /*evento de clique a todos os link com # quando clicado correspondente a uma página de com efeito deslizante*/
-}
+
     document.addEventListener("DOMContentLoaded", function () {
         document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             anchor.addEventListener('click', function (e) {
